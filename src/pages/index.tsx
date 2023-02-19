62a093584218215d0737072863bb17280bc33f50
Glecio Santos <glecio.santos@gmail.com>
2023-02-19 18:34:32 -0300

    Fix set current section based on scroll position

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,13 +19,12 @@ export default function Home() {
     const sections = document.querySelectorAll <HTMLElement>('section[id]')
     function scrollActive(){
       const scrollY = window.pageYOffset
-
+      
       sections.forEach(current =>{
           const sectionHeight = current.offsetHeight,
                 sectionTop = current.offsetTop - 50,
                 sectionId = current.getAttribute('id')
-
-          if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+          if(scrollY+300 > sectionTop && scrollY <= sectionTop + sectionHeight){
               document.querySelector('.nav__menu a[href*=' + sectionId + ']')!.classList.add('active-link')!
           }else{
               document.querySelector('.nav__menu a[href*=' + sectionId + ']')!.classList.remove('active-link')!
