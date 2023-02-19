de1152ce0a2d7e4b57bdcc746ebb315726182a53
Glecio Santos <glecio.santos@gmail.com>
2023-02-19 17:55:57 -0300

    Footer size

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
+import {Footer} from '../components/Footer'
 export default function Document() {
   return (
     <Html lang="pt-br">
@@ -8,6 +8,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Footer/>
       </body>
     </Html>
   )
